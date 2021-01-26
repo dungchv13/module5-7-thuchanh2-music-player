beb7360e45d76670de0a0f6391186e4ce03e150b
unknown <dungutc13@gmail.com>
2021-01-26 21:51:23 +0700

    youtube video

---
+++ src/app/youtube-playlist/youtube-playlist.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {YoutubeService} from '../youtube.service';
+
+@Component({
+  selector: 'app-youtube-playlist',
+  templateUrl: './youtube-playlist.component.html',
+  styleUrls: ['./youtube-playlist.component.css']
+})
+export class YoutubePlaylistComponent implements OnInit {
+  playlist: any[] = [];
+
+  constructor(public youtubeService: YoutubeService) { }
+
+  ngOnInit(): void {
+    this.playlist = this.youtubeService.playlist;
+  }
+
+}
